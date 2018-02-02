bd99db73f55a17dbd7115a9822bcb7b4d8f0520b
nsai17s-4 <nsai17s-4@sr.dmcs.pl>
2018-02-02 17:35:43 +0100

    pdf ogarniety?

--- grails-app/controllers/kamocinekfa/CoachController.groovy
+++ grails-app/controllers/kamocinekfa/CoachController.groovy
@@ -1,5 +1,18 @@
 package kamocinekfa
 
+import com.lowagie.text.Document
+import com.lowagie.text.Font
+import com.lowagie.text.FontFactory
+import com.lowagie.text.PageSize
+import com.lowagie.text.Paragraph
+import com.lowagie.text.Phrase
+import com.lowagie.text.pdf.BaseFont
+import com.lowagie.text.pdf.PdfPCell
+import com.lowagie.text.pdf.PdfPTable
+import com.lowagie.text.pdf.PdfWriter
+
+import java.text.SimpleDateFormat
+
 class CoachController {
 
     def index() {}
@@ -55,18 +68,80 @@ class CoachController {
     }
 //zmienic zeby brac po coachu
     def attendanceList () {
+
         def coach = Coach.findByName("Jacek")
-//        def file = new File("C:\\Users\\Mateusz\\Desktop\\cwiczenia\\react_warsztaty.pdf")
-//        render(/*template: '/templates/pdf/attendance', model: [coach: coach],*/file: file, fileName: coach.courses[0].title + ".pdf")
-//        new File("dupa.jpg").withOutputStream { outputStream ->
-//            pdfRenderingService.render([template: '/templates/pdf/attendance', model: [coach: coach]], outputStream)
-//        }
+        def course = coach.courses[0]
+
+        Document document = new Document()
+        String folderName = coach.name + "_" + coach.lastname + ".pdf"
+        def file = new File(folderName)
+        PdfWriter.getInstance(document, new FileOutputStream(file))
+        document.pageSize = PageSize.A4
+        final String FONT = "C:\\Windows\\Fonts\\Arial.ttf"
+//        Font font = FontFactory.getFont(FontFactory.HELVETICA)
+        BaseFont bf = BaseFont.createFont(FONT, BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+        Font font  = new Font(bf, 12)
+        document.open()
+
+        document.add(new Paragraph("Imie trenera: " + coach.name, font))
+        document.add(new Paragraph("Nazwisko trenera: " + coach.lastname, font))
+        document.add(new Paragraph("Nazwa kursu: " + course.title, font))
+        document.add(new Paragraph(" "))
+
+        document.add(new Paragraph("Lista obecności:", font))
+        document.add(new Paragraph(" "))
+        PdfPTable table = new PdfPTable(4)
+        PdfPCell cell = new PdfPCell()
+
+
+        cell.phrase = new Phrase("Imie", font)
+        table.addCell(cell)
+        cell.phrase = new Phrase("Nazwisko", font)
+        table.addCell(cell)
+        cell.phrase = new Phrase("Data", font)
+        table.addCell(cell)
+        cell.phrase = new Phrase("Podpis", font)
+        table.addCell(cell)
+
+        def format = new SimpleDateFormat("dd-MM-yyyy")
+        def date = new Date()
+        def dateS = format.format(date)
+
+        for (Player player in course.players) {
+            cell.phrase = new Phrase(player.name, font)
+            table.addCell(cell)
+            cell.phrase = new Phrase(player.lastname, font)
+            table.addCell(cell)
+            cell.phrase = new Phrase(dateS, font)
+            table.addCell(cell)
+            cell.phrase = new Phrase()
+            table.addCell(cell)
+        }
+
+        document.add(table)
+        document.close()
+        render(file: file, contentType: "application/pdf")
 
 
 
-        render(template: '/templates/pdf/attendance',
-                model: [coach: coach],
-                contentType: "application/pdf",
-                file: (new File("C:\\Users\\Mateusz\\Desktop\\test.pdf")).createNewFile())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//        render(template: '/templates/pdf/attendance',
+//                model: [coach: coach],
+//                contentType: "application/pdf",
+//                file: (new File("C:\\Users\\Mateusz\\Desktop\\test.pdf")).createNewFile())
     }
 }
